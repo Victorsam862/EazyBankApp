0ec7686726a8d505c2ea71b61edf962b647b51ac
Victorsam862 <150631843+Victorsam862@users.noreply.github.com>
2024-05-28 03:27:04 +0400

    my bank app

---
+++ app.js
@@ -0,0 +1,18 @@
+var toggleSwitch = document.getElementById('toggle');
+var body = document.bod;
+var header = document.getElementById('header','head');
+var container = document.getElementById('bod');
+
+toggleSwitch.addEventListener('change', ()=>{
+    if(toggleSwitch.checked){
+        container.style.backgroundColor = 'black'
+        header.style.background = 'hsl(220, 16%, 96%)'
+        container.style.color = 'hsl(233, 8%, 62%)'
+    }
+    else{
+        container.style.backgroundColor = 'white'
+        
+    }
+})
+
+
